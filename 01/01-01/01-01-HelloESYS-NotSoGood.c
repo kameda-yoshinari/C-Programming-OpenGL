2988f78e01ee2a388cd8b42f8abc8f2b581f9115
KAMEDA, Yoshinari <45651568+kameda-yoshinari@users.noreply.github.com>
2024-01-19 00:19:15 +0900

    Add files via upload

---
+++ 01/01-01/01-01-HelloESYS-NotSoGood.c
@@ -0,0 +1,13 @@
+﻿// Keisanki Joron 2 (Introduction to Computing II)
+// Dept. of Engineering Systems, University of Tsukuba
+// [UTF-8 / Unix]
+// 計算機序論２・実習 (筑波大学工学システム学類)
+
+// 2024/01/18a kameda[at]ccs.tsukuba.ac.jp
+// 2011/11/22a kameda[at]iit.tsukuba.ac.jp
+// 01.01 ぞんざいなHello ESYS
+
+main(){
+  printf("Hello, ESYS folks (This coding is very bad).\n");
+}
+
